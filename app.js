7bcf8e78c6577a838d9570e9495d9116803434b6
noarfarber <noaros88@gmail.com>
2022-06-11 09:29:01 +0100

    moving axios api request to a separate file

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const axios = require('axios');
 const path = require('path');
 const port = 3000;
-require('dotenv').config();
+const getData = require('./getData');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', async (req, res) => {
@@ -14,33 +14,10 @@ app.get('/', async (req, res) => {
 	const headlines = await getData;
 
 	res.render('index', {
-		title: 'Headlines',
+		title: 'FINANCIAL TIMES',
 		articles: headlines
 	});
 });
-
-
-const url = `https://api.ft.com/content/search/v1?apiKey=${process.env.ApiKey}`;
-
-const getData = axios.post(url,
-	{
-		"queryString": "banks",
-		"queryContext": {
-			"curations": [ "ARTICLES" ]
-		},
-		"resultContext" : {
-			"aspects": [ "title", "location", "summary" ]
-		}
-	},
-	{
-		'Content-Type': 'application/json'
-	},
-	).then((response) => {
-		const headlines = (response.data.results[0].results.map(obj => obj.title.title));
-		return headlines;
-	})
-	.catch((error) => console.log(error));	
-
 
 app.listen(port, () => {
 	console.log(`App is listening on port ${port}!`)
---
+++ getData.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const url = `https://api.ft.com/content/search/v1?apiKey=${process.env.ApiKey}`;
+
+const getData = axios.post(url,
+	{
+		"queryString": "banks",
+		"queryContext": {
+			"curations": [ "ARTICLES" ]
+		},
+		"resultContext" : {
+			"aspects": [ "title", "location", "summary" ]
+		}
+	},
+	{
+		'Content-Type': 'application/json'
+	},
+	).then((response) => {
+		const headlines = (response.data.results[0].results.map(obj => obj.title.title));
+		console.log(headlines)
+		return headlines;
+	})
+	.catch((error) => console.log(error));
+
+
+module.exports = getData;
